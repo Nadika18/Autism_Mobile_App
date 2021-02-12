e2a70fc0a055a2d5001c899d83cf45fc3c1d91c1
Krishbin Paudel <krishbin@onmail.com>
2021-02-13 00:34:21 +0545

    flutter: use utils.dart to store all the modular code Reference from a single multi-usage file rather than creating files for each and every function.

---
+++ lib/utils.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+Widget CircularImageContainer(String location,double size) {
+  double _size = size;
+  return Center(
+      child: new Container(
+          width: _size,
+          height: _size,
+          decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              image: new DecorationImage(
+                  fit: BoxFit.cover,
+                  image: new AssetImage(
+                      location )
+              )
+          )
+      ),
+  );
+}

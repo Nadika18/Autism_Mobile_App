63ffeaeb4938cf2ed7ba11859a100da8241317b3
Nadika <nadikapoudel51@gmail.com>
2021-02-18 19:41:42 +0545

    Create question and Questions_by_parents added

---
+++ lib/create_question.dart
@@ -0,0 +1,203 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CreateQuestion extends StatefulWidget {
+  @override
+  _CreateQuestionState createState() => _CreateQuestionState();
+}
+
+class _CreateQuestionState extends State<CreateQuestion> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+  String _question;
+  String _option1;
+  String _option2;
+  String _option3;
+  String _option4;
+
+  final GlobalKey<FormState> _formkey = GlobalKey();
+  Widget _buildQuestion(){
+    return Material(
+      child: TextFormField(
+      decoration: InputDecoration(
+        labelText: "What is the question?"),
+        validator: (String value){
+          if(value.isEmpty){
+            return "Question is Required";
+          }
+          return null;
+        } ,
+        onSaved: (String value){
+          _question = value;
+        }, 
+      ));
+    
+  }
+
+  Widget _buildOption1(){
+    return Material(
+      child: TextFormField(
+      decoration: InputDecoration(
+        labelText: "Option 1"),
+        validator: (String value){
+          if(value.isEmpty){
+            return "Option is Required";
+          }
+          return null;
+        } ,
+        onSaved: (String value){
+          _option1 = value;
+        }, 
+      ));
+  }
+
+  Widget _buildOption2(){
+    return Material(
+      child: TextFormField(
+      decoration: InputDecoration(
+        labelText: "Option 2"),
+        validator: (String value){
+          if(value.isEmpty){
+            return "Option is Required";
+          }
+          return null;
+        } ,
+        onSaved: (String value){
+          _option2 = value;
+        }, 
+      ));
+  }
+
+  Widget _buildOption3(){
+    return Material(
+      child: TextFormField(
+      decoration: InputDecoration(
+        labelText: "Option 3"),
+        validator: (String value){
+          if(value.isEmpty){
+            return "Option is Required";
+          }
+          return null;
+        } ,
+        onSaved: (String value){
+          _option3 = value;
+        }, 
+      ));
+  }
+
+  Widget _buildOption4(){
+    return Material(
+      child: TextFormField(
+      decoration: InputDecoration(
+        labelText: "Option 4"),
+        validator: (String value){
+          if(value.isEmpty){
+            return "Option is Required";
+          }
+          return null;
+        } ,
+        onSaved: (String value){
+          _option4 = value;
+        }, 
+      ));
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon:Icon(Icons.keyboard_arrow_left_sharp , color: Colors.black, size: 45)), 
+        backgroundColor: Colors.white, 
+        title: Text("Questions", style: TextStyle(color: Colors.black),),),
+      body: Container(
+        
+        padding: EdgeInsets.all(10),
+      child: Form(
+        key: _formkey,
+        child: Column(
+          
+          children: [
+            ListTile(title: _buildQuestion()),
+            Divider(color: Colors.white),
+            Container(child: Column(children: [
+              ListTile(title:Align(alignment: Alignment.centerLeft, child: Text("Options", style: TextStyle(color: Colors.black),))),
+              
+              Card(
+                child: Column(
+                  children: [
+                    ListTile(
+                      title:_buildOption1(), 
+                      trailing:OutlineButton(
+                        borderSide: BorderSide(color: Colors.blue[300]),
+                        textColor: Colors.blue[300],
+                        
+                        child: Text("+ Add Image "),
+                        onPressed: (){getImage();})),
+                        ListTile(
+                      title:_buildOption2(), 
+                      trailing:OutlineButton(
+                        borderSide: BorderSide(color: Colors.blue[300]),
+                        textColor: Colors.blue[300],
+                        
+                        child: Text("+ Add Image "),
+                        onPressed: (){})),
+                        ListTile(
+                      title:_buildOption3(), 
+                      trailing:OutlineButton(
+                        borderSide: BorderSide(color: Colors.blue[300]),
+                        textColor: Colors.blue[300],
+                        
+                        child: Text("+ Add Image "),
+                        onPressed: (){})),
+                        ListTile(
+                      title:_buildOption4(), 
+                      trailing:OutlineButton(
+                        borderSide: BorderSide(color: Colors.blue[300]),
+                        textColor: Colors.blue[300],
+                        
+                        child: Text("+ Add Image "),
+                        onPressed: (){})),
+           ],),)],)  )
+           ,
+           Divider(color: Colors.white),
+           CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.blue,
+                child: IconButton(
+              icon:Icon(Icons.done, color: Colors.white,),
+              
+            onPressed: (){
+              if(!_formkey.currentState.validate()){
+                return;
+              }
+              _formkey.currentState.save();
+              print(_question);
+              print(_option1);
+              print(_option2);
+              print(_option3);
+              print(_option4);
+              print(_image);
+              
+            })
+              ),
+              ])
+        )
+      
+    ));
+  }
+}
--- lib/questions_by_parents.dart
+++ lib/questions_by_parents.dart
@@ -1,1 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_view/quiz_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class QuestionsByParents extends StatefulWidget {
+  @override
+  _QuestionsByParentsState createState() => _QuestionsByParentsState();
+}
+
+class _QuestionsByParentsState extends State<QuestionsByParents> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              QuizView(
+                image: Container(
+                  height: 150,
+                  width: 140,
+                  child: Image.asset('assets/image2.png',fit: BoxFit.fill,),
+                ),
+                  showCorrect: true,
+                  questionTag: 'Question 1',
+                  questionColor: Colors.black,
+                  tagColor: Colors.yellowAccent,
+                  backgroundColor: Colors.white70,
+                  tagBackgroundColor: Colors.red.shade300,
+                  answerColor: Colors.white,
+                  answerBackgroundColor: Colors.deepPurple,
+                  question: 'What is the Color of the Player?',
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  rightAnswer: 'Green',
+                  wrongAnswers: ['Red','Yellow'],
+                  onRightAnswer: ()=> print('Right!'),
+                  onWrongAnswer: (){
+                  Alert(
+                      context: context,
+                      title: 'Wrong!',
+                  ).show();
+                  }
+              ),
+              SizedBox(height: 10,),
+              QuizView(
+                  image: Container(
+                    height: 150,
+                    width: 140,
+                    child: Image.asset('assets/image3.png',fit: BoxFit.fill,),
+                  ),
+                  showCorrect: true,
+                  questionTag: 'Question 2',
+                  questionColor: Colors.black,
+                  tagColor: Colors.yellowAccent,
+                  backgroundColor: Colors.white70,
+                  tagBackgroundColor: Colors.red.shade300,
+                  answerColor: Colors.white,
+                  answerBackgroundColor: Colors.deepPurple,
+                  question: 'What is the Color of the Player?',
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  rightAnswer: 'Red',
+                  wrongAnswers: ['Green','Yellow'],
+                  onRightAnswer: ()=> print('Right!'),
+                  onWrongAnswer: (){
+                    Alert(
+                      context: context,
+                      title: 'Wrong!',
+                    ).show();
+                  })
+            ],
+            ),
+          )
+      ),
+      
+    );
+  }
+}
